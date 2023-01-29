3eebaf4c119ea7198349af70b605b9707ba3cff7
Skippyturtle <57076195+Skippyturtle@users.noreply.github.com>
2023-01-29 23:49:15 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ import time
 
 while True:
 
-
   url_rewards = "https://xmr.2miners.com/api/accounts/45Jq4Gokx5BbwLnrLRUFquG4uSZ4mkcHDMYjibNBgrD291wG4Pz8bcx9KScWycUjW9iiejGY5PoQ9eMUsZaZ7Z6S1XggBKi"
   response_rewards = requests.get(url_rewards)
   data_rewards = response_rewards.json()
@@ -37,30 +36,40 @@ while True:
   content = "Last 24h reward number: 0.0{} XMR\nLast 24h reward number in EUR: {:.2f}\nActual Round Share: {}%\nNumber of workers online: {}\nLast Block XMR reward: 0.000{}\nTotal payout :{}\nCurrent Hashrate : {} KH/s\nAverage Hashrate : {} KH/s\nNetwork Difficulty: {} G".format(last_reward_num, eur, last_reward_percentage, workerOnline, Last_Block_XMR_reward, paymentsTotal, currentHashrate, Hashrate, difficulty)
 
   def send_to_discord(content):
-      webhook_url = "https://discord.com/api/webhooks/1069319353956323348/hbY_Ouh7lwSymAf9Lk6yE4e2tP-Fuaaq-axphwlgVgDxEsQxMdvdI5KqCpwuJfm0bDFF"
-      payload = {
-          "avatar_url": "https://cdn.iconscout.com/icon/free/png-256/monero-3629611-3032309.png",
-          "username": "2miners XMR pool",
-          "embeds": [
-              {
-                  "title": "2miners XMR Pool Statistics",
-                  "description": content,
-                  "color": 14177041,
-                  "footer": {
-                      "text": "By Skippyturtle#4893"
-                  }
-              }
-          ]
-      }
+    webhook_url = "https://discord.com/api/webhooks/1069319353956323348/hbY_Ouh7lwSymAf9Lk6yE4e2tP-Fuaaq-axphwlgVgDxEsQxMdvdI5KqCpwuJfm0bDFF"
+    payload = {
+        "avatar_url": "https://cdn.iconscout.com/icon/free/png-256/monero-3629611-3032309.png",
+        "username": "2miners XMR pool",
+        "embeds": [
+            {
+                "title": "2miners XMR Pool Statistics",
+                "description": content + "\n\n<@1056761423528722493>", 
+                "color": 14177041,
+                "allowed_mentions": {
+                   "parse": [
+                        "users"
+                    ],
+                    "users": [
+                        1056761423528722493
+                    ]
+                  },
+
+                "footer": {
+                    "text": "By Skippyturtle#4893"
+                }
+            }
+        ]
+    }
 
 
-      headers = {"Content-Type": "application/json"}
-      try:
-          response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
-          response.raise_for_status()
-          print("Payload sent to Discord successfully")
-      except requests.exceptions.RequestException as error:
-          print(f"Failed to send payload to Discord: {error}")
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+        response.raise_for_status()
+        print("Payload sent to Discord successfully")
+    except requests.exceptions.RequestException as error:
+        print("Failed to send payload to Discord: {0}".format(error))
+
 
   send_to_discord(content)
   print(content)
